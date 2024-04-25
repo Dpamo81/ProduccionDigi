cdcbbafff727dbc87e286b252b5482017ab123e0
Dani <dpajaresfo@gmail.com>
2024-04-25 16:01:36 +0200

    Model_1

---
+++ Production_Digi/Models/Production_Digi_Models.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Production_Digi.Models
+{
+    public class Campo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+    }
+    public class Registro
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public int CampoId { get; set; }
+
+        public Campo Campo { get; set; }
+        public double valor { get; set; }
+    }
+
+
+}
